877bf877f5bf6ab88d29e9e322c4a2fe33e27f70
Nikolas Silva de Matos <nikolas.matos@edu.senai.br>
2026-09-25 10:27:26 -0300

    Lição js

--- Licao.js
+++ Licao.js
@@ -1,1 +1,95 @@
+const usuario = {
+  login: "admin",
+  senha: "1234",
+  tipo: "administrador"
+};
+
+console.log(`O login do ${usuario.tipo} é ${usuario.login} e a senha é ${usuario.senha}, muito fraca por sinal.`);
+
+const aluno = {
+  nome: "Nikolas Silva",
+  curso: "Desenvolvimento de Sistemas",
+  turma: "2º DS"
+};
+
+console.log(`O aluno ${aluno.nome} está matriculado no curso de ${aluno.curso}, na turma ${aluno.turma}.`);
+
+
+const professor = {
+  nome: "Eduardo",
+  disciplina: "JavaScript",
+  turno: "manhã"
+};
+
+console.log(`O professor ${professor.nome} ministra a disciplina de ${professor.disciplina} no turno da ${professor.turno}.`);
+
+const empresa = {
+  nome: "Tech Solutions",
+  area: "Tecnologia",
+  cidade: "São Paulo" 
+};
+
+console.log(`A empresa ${empresa.nome} atua na área de ${empresa.area} e fica em ${empresa.cidade}.`);
+
+
+const computador = {
+  marca: "Dell",
+  processador: "Intel i5",
+  memoria: "8GB"
+};
+
+console.log(`O computador da marca ${computador.marca} possui processador ${computador.processador} e memória de ${computador.memoria}.`);
+
+
+
+const livro = {
+  titulo: "JavaScript Básico",
+  autor: "Ana Silva",
+  paginas: 200
+};
+
+console.log(`O livro ${livro.titulo} foi escrito por ${livro.autor} e possui ${livro.paginas} páginas.`);
+
+
+
+const filme = {
+  nome: "Matrix",
+  genero: "Ficção",
+  ano: 1999
+};
+
+console.log(`O filme ${filme.nome} é do gênero ${filme.genero} e foi lançado em ${filme.ano}.`);
+
+const conta = {
+  usuario: "eduardo",
+  senha: "1234",
+  nivel: "administrador"
+};
+
+console.log(`O usuário ${conta.usuario} possui nível ${conta.nivel} e utiliza a senha ${conta.senha}, muito fraca por sinal.`);
+
+
+const escola  = { 
+
+  Nome: "Escola Sesi",
+  cidade: "Guarulhos",
+  turno: "Manhã" 
+
+
+
+}; 
+
+console.log(`A escola ${escola.nome} está localizada na cidade de ${escola.cidade} e funciona no turno da ${escola.turno}.`);
+
+
+const brasil = {
+ Eleitores: 1000000,        
+ Votaram: 800000, 
+ NãoVotaram: 200000, 
+
+
+
+}; 
+
+console.log(`No Brasil, existem ${brasil.Eleitores} eleitores, dos quais ${brasil.Votaram} votaram e ${brasil.NãoVotaram} não votaram.`);
 
